6b05be21d55e98ab42b1c87c91ce9fc2a6cc4919
Carsten Scheuer <scheuer@inveris.de>
2023-05-19 11:37:49 +0200

    fix: legacy plugin support
     - add render method for "legacy plugin" license status form elements
       to settings helper class
     - bump stable version to 1.8.2
     - rename development branch folder to DEV_4
     - replace latest production branch folder by new version 1.8.2

---
+++ src/V1_8_2/class-remote-fs-utils.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Class Remote_FS_Utils
+ *
+ * @package immonex\WordPressFreePluginCore
+ */
+
+namespace immonex\WordPressFreePluginCore\V1_8_2;
+
+/**
+ * Remote filesystems related utilities.
+ */
+class Remote_FS_Utils {
+
+
+
+} // class Remote_FS_Utils
--- tests/immonex-test-plugin/includes/class-test-plugin.php
+++ tests/immonex-test-plugin/includes/class-test-plugin.php
@@ -10,7 +10,7 @@ namespace myimmonex\TestPlugin;
 /**
  * Main test plugin class.
  */
-class Test_Plugin extends \immonex\WordPressFreePluginCore\DEV_3\Base {
+class Test_Plugin extends \immonex\WordPressFreePluginCore\DEV_4\Base {
 
 	const
 		PLUGIN_NAME = 'My immonex Test Plugin',
