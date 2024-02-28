88da1c3d5c8b212b12d4a5c2d4604c91641dc8d6
Carsten Scheuer <scheuer@inveris.de>
2024-02-28 16:20:36 +0100

    improvement: HTML to plain text conversion
     - extend HTML to plain text conversion method (string utils)
     - rename dev branch folder to DEV_3
     - bump stable version to 1.9.8 (replace previous production branch
       folder)
     - update dependencies

--- examples/myimmonex-example-plugin/includes/class-example-plugin.php
+++ examples/myimmonex-example-plugin/includes/class-example-plugin.php
@@ -10,7 +10,7 @@ namespace myimmonex\ExamplePlugin;
 /**
  * Main plugin class.
  */
-class Example_Plugin extends \immonex\WordPressFreePluginCore\V1_9_7\Base {
+class Example_Plugin extends \immonex\WordPressFreePluginCore\V1_9_8\Base {
 
 	const PLUGIN_NAME                = 'My immonex Example Plugin';
 	const PLUGIN_PREFIX              = 'myplugin_';
