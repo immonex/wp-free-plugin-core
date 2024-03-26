5cf18ccc35bdae65c2a4f5ebe27b3fb1107cb6f4
Carsten Scheuer <scheuer@inveris.de>
2024-03-26 07:28:00 +0100

    fix: Twig template file loading
     - add file's own directory to Twig template search paths if an
       absolute path is given
     - rename dev branch folder to DEV_4
     - bump stable version to 1.9.10 (replace 1.9.9 production branch
       folder)
     - update dependencies

--- tests/immonex-test-plugin/includes/class-test-plugin.php
+++ tests/immonex-test-plugin/includes/class-test-plugin.php
@@ -10,7 +10,7 @@ namespace myimmonex\TestPlugin;
 /**
  * Main test plugin class.
  */
-class Test_Plugin extends \immonex\WordPressFreePluginCore\V1_9_9\Base {
+class Test_Plugin extends \immonex\WordPressFreePluginCore\V1_9_10\Base {
 
 	const PLUGIN_NAME    = 'My immonex Test Plugin';
 	const PLUGIN_PREFIX  = 'testplugin_';
