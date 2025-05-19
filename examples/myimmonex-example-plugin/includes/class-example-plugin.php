921c82015f391857869519f0bf1b7d039f2d2034
Carsten Scheuer <scheuer@inveris.de>
2025-05-19 08:38:50 +0200

    improvement: type check in is_local_url()
     - add param type check to is_local_url method (string utils)
     - update dependencies
     - rename development branch folder to DEV_5
     - bump stable version to 2.3.1 (replace current production branch
       folder)

--- examples/myimmonex-example-plugin/includes/class-example-plugin.php
+++ examples/myimmonex-example-plugin/includes/class-example-plugin.php
@@ -10,7 +10,7 @@ namespace myimmonex\ExamplePlugin;
 /**
  * Main plugin class.
  */
-class Example_Plugin extends \immonex\WordPressFreePluginCore\V2_3_0\Base {
+class Example_Plugin extends \immonex\WordPressFreePluginCore\V2_3_1\Base {
 
 	const PLUGIN_NAME                = 'My immonex Example Plugin';
 	const PLUGIN_PREFIX              = 'myplugin_';
